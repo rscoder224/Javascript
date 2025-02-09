835d24d7174e216871b91ae210660b874ee1fcba
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-02-09 21:56:52 +0530

    Create practice_04.js

---
+++ practice_04.js
@@ -0,0 +1,25 @@
+//  Boolean returns tyrue or false from
+
+//  ==
+
+// ===
+
+// ==!
+
+// =!
+
+// Practicing If else condition in JavaScript
+
+age = 17;
+
+if(age<=12){
+    console.log("user cannot play pubg");
+}else if(age < 18 ) {
+    console.log("user can play user can play pubg for 2 hours only ");
+    
+}else{
+    console.log("Use can play pubg for more than 2 hours");
+};
+
+
+
