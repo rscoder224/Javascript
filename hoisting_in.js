8308c3203c59d4d13d6578380b56303f8d29cbc2
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-30 12:56:14 +0530

    Create hoisting_in.js
    hoistion in javaScript

---
+++ hoisting_in.js
@@ -0,0 +1,16 @@
+// hoisting in javaScript
+// declare krne se pahele agar function ko call kiya toh function declarartion me toh work karega but arrow function or function experssion me nhi ishi behaviour ko hoisting kahete hai
+
+hello();
+function  hello(){
+    console.log("Hello bro how are you");
+};
+
+
+
+
+
+
+
+
+
