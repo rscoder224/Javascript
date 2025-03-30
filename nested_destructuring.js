47181ddbc5091b219addfbf0a46fa6b13d7d2c3b
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-30 15:39:56 +0530

    Update nested_destructuring.js

--- nested_destructuring.js
+++ nested_destructuring.js
@@ -7,6 +7,19 @@ const [{userName : userName_user1},{userId: _user2}] = users;
 console.log(userName_user1);
 console.log(_user2);
 
+// practicle Implimentation
+
+let arr1 = [{userName: "Shaurya" ,password: "gdvg37dg" , mobile: 030489848   },
+{userName: "Divyanshu" ,password: "ed7348tgd", mobile:9348390584   },
+{userName: "Ayush" ,password: "sg3ry11" , mobile: 8928388349   }];
+
+
+let[{userName:myname, password : s_pass},{password:d_pass},{mobile:a_phone}] = arr1;
+
+console.log(myname);
+console.log(d_pass);
+console.log(a_phone);
+console.log(s_pass);
 
 
 
