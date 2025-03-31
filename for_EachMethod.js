604f17b4cc48d092d5b5a2f4f1624140b58c2fbc
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-31 08:14:43 +0530

    Create for_EachMethod.js
    // forEach method of arrays

---
+++ for_EachMethod.js
@@ -0,0 +1,7 @@
+// forEach method of arrays
+// forEcah not creates an array
+const nums = [2,3,4,5,6,7,8];
+
+nums.forEach((number,index)=>{
+    console.log(`index is ${index} and number is ${number} hence  ${number} X 2 is  ${number*2}`)
+});
