9316963e3aafb2c65af08b2405fa3f6eb0260f3c
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-01 22:17:21 +0530

    Create iterableInjavaScript.js
    // iterables
    // jispe hum log for of loop sake;
    // string , array are iterable
    
    // array like objects 
    // jiske pass length property hoti hai
    // example:- String

---
+++ iterableInjavaScript.js
@@ -0,0 +1,21 @@
+// iterables
+// jispe hum log for of loop sake;
+// string , array are iterable
+
+// array like objects 
+// jiske pass length property hoti hai
+// example:- String
+
+const array = ['item1','item2','item3'];
+
+for(let item of array){
+    console.log(array);
+}
+
+const name = "shaurya";
+
+for(let char of name){
+    console.log(char);
+};
+
+
