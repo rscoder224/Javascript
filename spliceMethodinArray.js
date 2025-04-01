22d1654cc263da2861d5ff6e99f998be4113354a
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-01 16:46:34 +0530

    Create spliceMethodinArray.js
    // splice method of array 
    // Start Index , no. of items want to Delete , Insert

---
+++ spliceMethodinArray.js
@@ -0,0 +1,16 @@
+// splice method of array 
+// Start Index , no. of items want to Delete , Insert
+
+
+const myArray = ["item","item2","item3","item4"];
+myArray.splice(1,1);
+
+
+
+myArray.splice(1,0,"item2");
+
+console.log(myArray);
+
+
+
+
