835a3d9963642ee383fb83c798543986b9292cef
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-01 16:38:07 +0530

    Update fillMethodOfArray.js
    // fill method inside array
    // value ,start index , end index

--- fillMethodOfArray.js
+++ fillMethodOfArray.js
@@ -2,6 +2,7 @@
 // fill method inside array
 // value ,start index , end index
 
+// it creates an array of 10 indexes where all 10 are filled with -1
 const myArray = new Array(10).fill(-1);
 console.log(myArray);
 
@@ -13,6 +14,7 @@ console.log(array1);
 
 const myarry2 = [1,2,3,4,5,6,7,8,9];
 
+// this will change original Array
 myarry2.fill(-1,5,8);
 
 
