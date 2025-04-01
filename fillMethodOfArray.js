23d10b69d0f75cb8db3a2947906a062569c124bb
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-01 16:36:21 +0530

    Create fillMethodOfArray.js
    // fill method inside array
    // value ,start index , end index

---
+++ fillMethodOfArray.js
@@ -0,0 +1,24 @@
+
+// fill method inside array
+// value ,start index , end index
+
+const myArray = new Array(10).fill(-1);
+console.log(myArray);
+
+
+const array1 = new Array(40).fill(45);
+
+console.log(array1);
+
+
+const myarry2 = [1,2,3,4,5,6,7,8,9];
+
+myarry2.fill(-1,5,8);
+
+
+console.log(myarry2);
+
+
+
+
+
