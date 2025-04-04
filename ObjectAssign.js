17b9fe9f6945462976bddb4fde9f91a14c36121b
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-04 10:06:13 +0530

    Create ObjectAssign.js
    Using object assign

---
+++ ObjectAssign.js
@@ -0,0 +1,13 @@
+// cloning object using Object.assign({}, objName)
+
+
+const obj1 = {
+name : 'shaurya',
+room : 'CS35',
+phone_number : 933483940,
+}
+
+
+const obj2 = Object.assign({},obj1);
+
+console.log(obj2);
