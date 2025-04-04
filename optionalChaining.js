785360dadd710da335d7975e101fe378f19461ad
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-04 10:16:28 +0530

    Create optionalChaining.js
    // optional Chaining it didn't return error

---
+++ optionalChaining.js
@@ -0,0 +1,12 @@
+// optional Chaining
+
+const user = {
+    firstName : 'Shaurya',
+    // address : {house_no : '92203'}
+}
+
+console.log(user?.firstName);
+console.log(user?.address?.house_no);
+
+// optional Chaining it didn't return error 
+
