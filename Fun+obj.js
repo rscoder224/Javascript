d36d871573a7508e8af123ddabecde0835e785d1
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-06 15:57:05 +0530

    Create Fun+obj.js
    function in JavaScript Extra Analysis

---
+++ Fun+obj.js
@@ -0,0 +1,17 @@
+// function in JavaScript Extra Analysis
+
+function hello() {
+console.log("hello Bro How are you are you getting Fine ")
+}
+
+
+
+// JavaScript Function are  ====>  functions + Objects
+// you can add your own properties 
+// example Like --:  
+
+hello.ownproperty = ["Hareraam"];
+
+
+console.log(hello.ownproperty);
+
