b5c9e2ee00bfc257eff9d1768c3de243e1ade868
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-04-18 22:38:14 +0530

    Create 1dom.js

---
+++ dom/1dom.js
@@ -0,0 +1,6 @@
+const main_heading = document.querySelector('#head');
+
+// main_heading.textContent = 'Hello World!';
+// main_heading.innerHTML = 'baby shark doo doo doo doo doo doo';
+main_heading.innerText = 'Hello World!';
+
