221b30e58e41def16aced76ec1c88d3c4fb820eb
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-04-19 22:35:51 +0530

    Create dom6.js

---
+++ dom/dom6.js
@@ -0,0 +1,24 @@
+
+// const head = document.querySelector('.header');
+
+// console.log(head.classList);
+
+
+// head.classList.add('bg-dark');
+
+// const head = document.querySelector('.header');
+
+// head.classList.remove('bg-dark');
+
+// head.classList.toggle('bg-dark');
+
+
+
+
+// document.createElement()
+// append
+// prepend
+// remove
+
+const newtodo = document.createElement('li');
+
